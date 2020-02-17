d824528e5f55d2689f555bf5e3ce307136acd7ec
Laszlo Varga <vargalaszlo1981@googlemail.com>
2020-02-17 01:01:45 +0000

    nem movie-search project

---
+++ public/apps/movie-search/precache-manifest.2880846887ea3ee9666c91a97b1fe745.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c72f66889e08fd02d0383ab2946443b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26e44e48b18ee46fe015",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "2184844b0ed57f422c07",
+    "url": "/static/css/main.3b6b4e95.chunk.css"
+  },
+  {
+    "revision": "26e44e48b18ee46fe015",
+    "url": "/static/js/2.ec9c4fcc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ec9c4fcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2184844b0ed57f422c07",
+    "url": "/static/js/main.56dbdc5e.chunk.js"
+  },
+  {
+    "revision": "a7fe8dab1ebb7d019fce",
+    "url": "/static/js/runtime-main.e3a98b59.js"
+  }
+]);
