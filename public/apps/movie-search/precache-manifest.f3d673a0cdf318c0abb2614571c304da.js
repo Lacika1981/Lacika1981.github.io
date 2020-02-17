5d6561ae2d379a636c6d8e262799dcc50e0c476c
Laszlo Varga <vargalaszlo1981@googlemail.com>
2020-02-17 01:20:10 +0000

    path not right

---
+++ public/apps/movie-search/precache-manifest.f3d673a0cdf318c0abb2614571c304da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c53b94a9284c5f8be698fcad34dbb68f",
+    "url": "/public/index.html"
+  },
+  {
+    "revision": "26e44e48b18ee46fe015",
+    "url": "/public/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e99cdc868d4ca2957596",
+    "url": "/public/static/css/main.3b6b4e95.chunk.css"
+  },
+  {
+    "revision": "26e44e48b18ee46fe015",
+    "url": "/public/static/js/2.ec9c4fcc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/public/static/js/2.ec9c4fcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e99cdc868d4ca2957596",
+    "url": "/public/static/js/main.1ddebb73.chunk.js"
+  },
+  {
+    "revision": "dcee90816527a4c5d034",
+    "url": "/public/static/js/runtime-main.66b7a094.js"
+  }
+]);
